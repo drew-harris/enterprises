f9879a1691b26065bf625b9d065e9996e0b1040c
Drew Harris <harrisd@smu.edu>
2023-11-24 21:00:43 -0600

    Clean logs

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -8,7 +8,6 @@ export const authRoutes = new Elysia()
   .get("/signin", async ({ set }) => {
     // TODO :Implement state cookie
     const [url, state] = await githubAuth.getAuthorizationUrl();
-    console.log("URL!!!", url);
     set.redirect = url.toString();
     return;
   })
@@ -18,14 +17,9 @@ export const authRoutes = new Elysia()
       return;
     }
 
-    console.log("CODE: ", query.code);
-
-    console.log("Test");
     try {
       const { getExistingUser, githubUser, createUser } =
         await githubAuth.validateCallback(query.code);
-
-      console.log("Validated callback");
 
       const getUser = async () => {
         const existingUser = await getExistingUser();
@@ -40,27 +34,17 @@ export const authRoutes = new Elysia()
 
       const user = await getUser();
 
-      console.log("Got user");
-
       const session = await auth.createSession({
         userId: user.userId,
         attributes: {},
       });
 
-      console.log("Created session");
       const sessionCookie = auth.createSessionCookie(session);
       set.headers["Set-Cookie"] = sessionCookie.serialize();
 
       set.redirect = "/";
     } catch (error) {
       console.error(error);
-      if (error instanceof LuciaError) {
-        console.log(error.stack);
-        console.log(error.name);
-        console.log(error.detail);
-        console.log(error.cause);
-        console.log(error.message);
-      }
       set.status = 500;
       return <div>Something went wrong</div>;
     }
